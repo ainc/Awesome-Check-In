b11c8982c1a2874e4c2069e04f325a4800a0dbb1
Brendan Thompson <brenthompson2@gmail.com>
2017-09-29 16:40:04 -0400

    First Production Version

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,20 @@
+// TypeScript for HomePage
+// Created By: Brendan Thompson
+// Updated: 09/10/17 by Brendan Thompson
+
+// Description:
+//		Asks the User to select what brought them in: Entrepreneurship, Learning To Code, or The Workspace
+//		If Entrepreneurship, directs to ProgramPage
+//		Else directs them to the TeamMembersPage
+
+
+// ==============================================================================
+// 		Import Navigation tools and ProgramPage & TeamMembersPage
+// ==============================================================================
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
 import { ProgramPage } from '../programs/program';
 import { TeamMembersPage } from '../teamMembers/teamMembers';
 
@@ -13,22 +28,31 @@ export class HomePage {
 
 	}
 
+	// ==============================================================================
+	// 		Entrepreneurship
+	// ==============================================================================
 	clickedEnt() {
 		this.navCtrl.push(ProgramPage);
 	}
 
+	// ==============================================================================
+	// 		Learning to Code
+	// ==============================================================================
 	clickedCode() {
 		var program = {
 			id: 7,
 			name:'Awesome Inc University',
 			description: 'The Bootcamps and Classes for both youth and adults',
-			imageURL: 'assets/img/AIncU_logo.png',
+			imageURL: 'assets/img/Awesome Inc U - Logo_header.png',
 			imageAlt: 'Awesome Inc U Logo'
 		};
 
 		this.navCtrl.push(TeamMembersPage, { currentProgram: program });
 	}
 
+	// ==============================================================================
+	// 		The Workspace
+	// ==============================================================================
 	clickedSpace() {
 		var program = {
 			id: 8,
