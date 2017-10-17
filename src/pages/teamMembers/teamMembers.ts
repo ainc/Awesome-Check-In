1da53400c402b852e6f6d3daf19fa8a32228eed8
brenthompson2 <brenthompson2@gmail.com>
2017-10-17 18:39:41 -0400

    Re-ordered Team Members

--- src/pages/teamMembers/teamMembers.ts
+++ src/pages/teamMembers/teamMembers.ts
@@ -1,6 +1,6 @@
 // TypeScript for TeamMemberPage
 // Created By: Brendan Thompson
-// Updated: 09/29/17 by Brendan Thompson
+// Updated: 10/17/17 by Brendan Thompson
 
 // Description:
 // 		Asks the user to fill out a FormGroup regarding which TEAMMEMBERS they are expecting to meet with
@@ -27,7 +27,15 @@ export class TeamMembersPage {
 	public currentProgram;
 	public currentMemberFormGroup : FormGroup;
 	public TEAMMEMBERS = [
-		{id: 1, name:'Emily Wehrle',
+		{id: 1, name:'Nobody Yet',
+			tag: 'Nobody',
+			description: 'Set up a meeting',
+			team: 'Team Alpha',
+			slackUsername: '#space',
+			imageURL: 'assets/img/TeamMembers/teamMember.png',
+			imageAlt: 'Team Member',
+		},
+		{id: 2, name:'Emily Wehrle',
 			tag: 'Emily',
 			description: 'Director of Operations',
 			team: 'main',
@@ -35,7 +43,7 @@ export class TeamMembersPage {
 			imageURL: 'assets/img/TeamMembers/emilywehrle_wall.gif',
 			imageAlt: 'Emily Wehrle'
 		},
-		{id: 2, name:'Brian Raney',
+		{id: 3, name:'Brian Raney',
 			tag: 'Brian',
 			description: 'Co-Founder',
 			team: 'main',
@@ -43,7 +51,7 @@ export class TeamMembersPage {
 			imageURL: 'assets/img/TeamMembers/brianraney_wall.gif',
 			imageAlt: 'Brian Raney'
 		},
-		{id: 3, name:'Amanda Murray',
+		{id: 4, name:'Amanda Murray',
 			tag: 'Amanda',
 			description: 'Director of Marketing',
 			team: 'Marketing',
@@ -51,7 +59,7 @@ export class TeamMembersPage {
 			imageURL: 'assets/img/TeamMembers/teamMember.png',
 			imageAlt: 'Amanda Murray'
 		},
-		{id: 4, name:'Keith McMunn',
+		{id: 5, name:'Keith McMunn',
 			tag: 'Keith',
 			description: 'Director of Fellowship',
 			team: 'Fellowship',
@@ -59,7 +67,7 @@ export class TeamMembersPage {
 			imageURL: 'assets/img/TeamMembers/teamMember.png',
 			imageAlt: 'Keith McMunn'
 		},
-		{id: 5, name:'Nick Such',
+		{id: 6, name:'Nick Such',
 			tag: 'Nick',
 			description: 'Co-Founder',
 			team: 'main',
@@ -67,21 +75,13 @@ export class TeamMembersPage {
 			imageURL: 'assets/img/TeamMembers/nicksuch_wall.gif',
 			imageAlt: 'Nick Such'
 		},
-		{id: 6, name:'Kyle Raney',
+		{id: 7, name:'Kyle Raney',
 			tag: 'Kyle',
 			description: 'Development Team',
 			team: 'Team Alpha',
 			slackUsername: '@raney24',
 			imageURL: 'assets/img/TeamMembers/kyleraney_wall.gif',
 			imageAlt: 'Kyle Raney'
-		},
-		{id: 7, name:'Nobody Yet',
-			tag: 'Nobody',
-			description: 'Set up a meeting',
-			team: 'Team Alpha',
-			slackUsername: '#bot_test',
-			imageURL: 'assets/img/TeamMembers/teamMember.png',
-			imageAlt: 'Team Member'
 		}
 	];
 
@@ -97,13 +97,13 @@ export class TeamMembersPage {
 
 		this.currentMemberFormGroup = this.memberFormBuilder.group ({
 			'teamMembers': this.memberFormBuilder.group({
+				Nobody	: [false],
 				Emily	: [false],
 				Brian	: [false],
 				Amanda	: [false],
 				Keith	: [false],
 				Nick	: [false],
-				Kyle	: [false],
-				Nobody	: [false]
+				Kyle	: [false]
 			}, { validator: this.memberValidator.validateABoxChecked })
 		});
 	}
