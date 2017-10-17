dce85e01c3360caadfdca49c954699995524d2c6
brenthompson2 <brenthompson2@gmail.com>
2017-10-17 18:39:08 -0400

    Changed Submti Button Text and Order of Team Members

--- src/pages/confirm/confirm.ts
+++ src/pages/confirm/confirm.ts
@@ -1,6 +1,6 @@
 // TypeScript for ConfirmPage
 // Created By: Brendan Thompson
-// Updated: 09/27/17 by Brendan Thompson
+// Updated: 10/17/17 by Brendan Thompson
 
 // Description:
 // 		Asks the User to confirm their user info and selected team members
@@ -30,7 +30,15 @@ export class ConfirmPage {
 		private userReason;
 	private ourHttp: Http;
 	public TEAMMEMBERS = [
-		{id: 1, name:'Emily Wehrle',
+		{id: 1, name:'Nobody Yet',
+			tag: 'Nobody',
+			description: 'Set up a meeting',
+			team: 'Team Alpha',
+			slackUsername: '#space',
+			imageURL: 'assets/img/TeamMembers/teamMember.png',
+			imageAlt: 'Team Member',
+		},
+		{id: 2, name:'Emily Wehrle',
 			tag: 'Emily',
 			description: 'Director of Operations',
 			team: 'main',
@@ -38,7 +46,7 @@ export class ConfirmPage {
 			imageURL: 'assets/img/TeamMembers/emilywehrle_wall.gif',
 			imageAlt: 'Emily Wehrle'
 		},
-		{id: 2, name:'Brian Raney',
+		{id: 3, name:'Brian Raney',
 			tag: 'Brian',
 			description: 'Co-Founder',
 			team: 'main',
@@ -46,7 +54,7 @@ export class ConfirmPage {
 			imageURL: 'assets/img/TeamMembers/brianraney_wall.gif',
 			imageAlt: 'Brian Raney'
 		},
-		{id: 3, name:'Amanda Murray',
+		{id: 4, name:'Amanda Murray',
 			tag: 'Amanda',
 			description: 'Director of Marketing',
 			team: 'Marketing',
@@ -54,7 +62,7 @@ export class ConfirmPage {
 			imageURL: 'assets/img/TeamMembers/teamMember.png',
 			imageAlt: 'Amanda Murray'
 		},
-		{id: 4, name:'Keith McMunn',
+		{id: 5, name:'Keith McMunn',
 			tag: 'Keith',
 			description: 'Director of Fellowship',
 			team: 'Fellowship',
@@ -62,7 +70,7 @@ export class ConfirmPage {
 			imageURL: 'assets/img/TeamMembers/teamMember.png',
 			imageAlt: 'Keith McMunn'
 		},
-		{id: 5, name:'Nick Such',
+		{id: 6, name:'Nick Such',
 			tag: 'Nick',
 			description: 'Co-Founder',
 			team: 'main',
@@ -70,21 +78,13 @@ export class ConfirmPage {
 			imageURL: 'assets/img/TeamMembers/nicksuch_wall.gif',
 			imageAlt: 'Nick Such'
 		},
-		{id: 6, name:'Kyle Raney',
+		{id: 7, name:'Kyle Raney',
 			tag: 'Kyle',
 			description: 'Development Team',
 			team: 'Team Alpha',
 			slackUsername: '@raney24',
 			imageURL: 'assets/img/TeamMembers/kyleraney_wall.gif',
 			imageAlt: 'Kyle Raney'
-		},
-		{id: 7, name:'Nobody Yet',
-			tag: 'Nobody',
-			description: 'Set up a meeting',
-			team: 'Team Alpha',
-			slackUsername: '#bot_test',
-			imageURL: 'assets/img/TeamMembers/teamMember.png',
-			imageAlt: 'Team Member'
 		}
 	];
 
